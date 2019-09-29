f1b5bfaf915490272a601e225d9ac754d7c430f5
blothe <b.lothe@codeur.online>
2019-09-29 16:32:17 +0200

    Add article_slidercard.php + include into index.php

---
+++ article_slidercard.php
@@ -0,0 +1,43 @@
+<?php
+
+// identification bdd
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mcs_sbj";
+// connexion à la bdd + récupération des données
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $stmt = $conn->query("SELECT `article_pic` AS `illustration`, `article_title` AS `titre`, `article_author` AS `auteur`, DATE_FORMAT(`article_datetime`, 'le %d/%m/%Y à %Hh%i') AS `publication` FROM `article` ORDER BY `ID` DESC LIMIT 10");
+  while ($result = $stmt->fetch()) {
+
+    ?>
+
+    <li onclick="location.href='#';" style="width: 240.4px; float: left; display: block;">
+      <img src="<?php echo $result['illustration'] ?>">
+      <div class="caption-info">
+        <div class="caption-info-head">
+          <div class="caption-info-head-left">
+            <h4><a href="#">Publié par <?php echo $result['auteur'] ?></a></h4>
+            <span><?php echo $result['publication'] ?></span>
+          </div>
+          <div class="caption-info-head-right">
+            <span> </span>
+          </div>
+          <div class="clear"> </div>
+        </div>
+      </div>
+    </li>
+
+    <?php
+
+  }
+}
+catch(PDOException $e)
+{
+  echo "Oups ! Échec de la connexion à la base de données :/" . $e->getMessage();
+}
+$conn = null;
+
+?>
